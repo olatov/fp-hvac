cb0dcded0f9f39b28d5b5df0f2c3be2fe0e44c64
Oleg Latov <oleg@tgadigital.com>
2024-06-16 00:29:43 +0300

    Wip

--- src/Hvac.Web/Hvac.Web.pp
+++ src/Hvac.Web/Hvac.Web.pp
@@ -12,9 +12,14 @@ uses
     Hvac.Models.Domain,
     Hvac.Models.Dto;
 
-const
-    ApiUrl: string = 'https://lazyjones.ddns.net/hvac/api/v1';
-    ApiKey: string = 'cd26c1f8-92e5-4460-b79b-ca707e5d4cef-9ad54c9c-63da-450f-a69a-05da0ef9d304';
+type
+    TSettings = record
+        ApiUrl: string;
+        ApiKey: string;
+    end;
+
+var
+    Settings: TSettings;
 
 procedure OnLoaded(AValue: JSValue); async;
 var
@@ -47,20 +52,38 @@ var
     headers, options: TJSObject;
     url: string;
 begin
-    headers := TJSObject.New();
-    headers['X-Api-Key'] := ApiKey;
+    url := Settings.ApiUrl + '/state';
+    options := new(['headers', new(['X-Api-Key', Settings.ApiKey])]);
 
-    options := TJSObject.New();
-    options['headers'] := Headers;
-
-    url := ApiUrl + '/state';
     Window.Fetch(url, options)._then(
         function(response: JSValue): JSValue begin OnLoaded(response) end,
         function(response: JSValue): JSValue begin OnError(response) end
     );
 end;
 
+procedure InitControls();
+var
+    mode: THvacMode;
+    selectElement: TJSHtmlSelectElement;
+    optionElement: TJSHtmlOptionElement;
+    item: TJSObject;
 begin
-    Writeln('Started');
+    selectElement := TJSHtmlSelectElement(
+        Document.GetElementById('mode')
+    );
+    selectElement.InnerHtml := '';
+
+    optionElement := TJSHtmlOptionElement(Document.CreateElement('option'));
+    item := new(['value', '0', 'text', 'Auto']);
+    optionElement.Value := IntToStr(Ord(THvacMode.mdAuto));
+    optionElement.Text := JS.ToString(item['text']);
+    selectElement.AppendChild(optionElement);
+end;
+
+begin
+    Settings.ApiUrl := 'https://lazyjones.ddns.net/hvac/api/v1';
+    Settings.ApiKey := 'cd26c1f8-92e5-4460-b79b-ca707e5d4cef-9ad54c9c-63da-450f-a69a-05da0ef9d304';
+
+    InitControls();
     Document.GetElementById('btnRefresh').AddEventListener('click', @Refresh);
 end.
